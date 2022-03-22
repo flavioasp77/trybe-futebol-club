31aca1d58af05537e3649849e3d95be52fb7ba58
Flavio Pires <flavioasp77@gmail.com>
2022-03-21 21:16:08 -0300

    Requisito 16 - Corrigindo erros nos tests

--- app/backend/src/database/routes/matchsRoute.ts
+++ app/backend/src/database/routes/matchsRoute.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
+// import validateJWT from '../auth/validateToken';
+import clubService from '../services/clubService';
 
 const matchsRouter = Router();
 
 matchsRouter.get(
   '/clubs',
+  // validateJWT,
+  clubService.getAll,
 );
 
 export default matchsRouter;
