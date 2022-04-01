2bd6a8a450ddbbec570b933e8281789b7c9ec3f5
Flavio Pires <flavioasp77@gmail.com>
2022-03-31 22:15:51 -0300

    Requisito 31 - criadas a fnção getAwayLeaderBoard da camada service

--- app/backend/src/database/services/leaderBoardService.ts
+++ app/backend/src/database/services/leaderBoardService.ts
@@ -3,8 +3,11 @@ import matchService from './matchService';
 import {
   clubStats,
   homeDraw,
+  awayDraw,
   homeLoss,
+  awayLoss,
   homeWin,
+  awayWin,
   classification,
 } from '../util/functions';
 
@@ -17,17 +20,34 @@ const getHomeLeaderBoard = async () => {
   matchs.forEach((match) => {
     if (!match.inProgress) {
       const score = match.homeTeamGoals - match.awayTeamGoals;
-      console.log('Score Service =====', score);
       if (score === 0) return homeDraw(match, leaderBoard);
       if (score < 0) return homeLoss(match, leaderBoard);
       return homeWin(match, leaderBoard);
     }
   });
   const homeClassification = classification(leaderBoard);
-  console.log('Service =========>', homeClassification);
+  return homeClassification;
+};
+
+const getAwayLeaderBoard = async () => {
+  const matchs = await matchService.getAll();
+  const clubs = await clubService.getAll();
+
+  const leaderBoard = clubStats(clubs as []);
+
+  matchs.forEach((match) => {
+    if (!match.inProgress) {
+      const score = match.homeTeamGoals - match.awayTeamGoals;
+      if (score === 0) return awayDraw(match, leaderBoard);
+      if (score < 0) return awayLoss(match, leaderBoard);
+      return awayWin(match, leaderBoard);
+    }
+  });
+  const homeClassification = classification(leaderBoard);
   return homeClassification;
 };
 
 export default {
   getHomeLeaderBoard,
+  getAwayLeaderBoard,
 };
